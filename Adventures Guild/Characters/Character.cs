ca6c678bec0278e30ff535e08240932be78ef66a
rubberduckster <mcrubygirl@hotmail.com>
2026-09-10 20:38:19 +0200

    Add project files.

---
+++ Adventures Guild/Characters/Character.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventures_Guild.Characters
+{
+    public abstract class Character
+    {
+        public string Name { get; }
+        public int Ascension { get; }
+        public bool IsAvailable { get; private set; }
+
+        public Character(string name, int ascension)
+        {
+            Name = name;
+            Ascension = ascension;
+            IsAvailable = true;
+        }
+
+        public abstract void UseElementalSkill();
+
+        public void SetUnavailable()
+        {
+            IsAvailable = false;
+        }
+
+        public void SetAvailable()
+        {
+            IsAvailable = true;
+        }
+    }
+}
